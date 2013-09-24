db8010058b4f3a10bb9a7e0b8998e49229977a52
tnedlacer <tnedlacer@gmail.com>
2013-09-24 23:06:29 +0900

    レイアウトを作成

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ PwExchange::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  root 'index#index'
 end
---
+++ spec/views/index/index.html.erb_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe "index/index.html.erb" do
+end
