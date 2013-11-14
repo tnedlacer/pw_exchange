1cb47c12a30abd3e010351a312aa96511daa1121
tnedlacer <tnedlacer@gmail.com>
2013-11-14 13:39:17 +0900

    鍵を管理するモデル

---
+++ app/models/key_manager.rb
@@ -0,0 +1,27 @@
+require 'singleton'
+class KeyManager
+  include Singleton
+  
+  attr_accessor :rsa
+  
+  def initialize
+    @rsa = OpenSSL::PKey::RSA.new(File.read(Rails.root.join("rsa/rsa_priv.pem")))
+  end
+  
+  def public_key
+    rsa.public_key.to_s
+  end
+  
+  [
+    [[:private_encrypt, :public_encrypt], :encode64],
+    [[:private_decrypt, :public_decrypt], :decode64], 
+  ].map do |rsa_methods, base64_method|
+    rsa_methods.map do |rsa_method|
+      define_method(rsa_method){|str| rsa.try(rsa_method, str).force_encoding(Encoding::UTF_8) }
+      define_method("#{rsa_method}_with_#{base64_method}"){|str|
+        self.try(rsa_method, Base64.try(base64_method, str))
+      }
+    end
+  end
+  
+end
